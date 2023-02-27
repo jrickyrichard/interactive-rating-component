95bfd5cc3ea4fc7c7362e7a4d857ddf5b257160b
jrickyrichard <jrrsum1010@gmail.com>
2023-02-27 12:37:02 +0800

    fix script.js

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ function submit(){
     let rating = document.querySelector('#rating');
     let thankYou = document.querySelector('#thank-you');
     let option = document.querySelector('#rating-option');
-    let small = document.querySelector('small')
-    let result;
+    let small = document.querySelector('small span');
+    
+    
+    Array.from(option.children).map(item => {
+        item.addEventListener('click', function(){
+            let result = item.textContent;
+            small.textContent = result;
+        })
+    })
 
     button.addEventListener('click', function() {
         rating.classList.add('display-none');
         thankYou.classList.remove('display-none');
-    })
-
-    Array.from(option.children).map(item => {
-        item.addEventListener('click', function(){
-            result = this.textContent;
-            return result;
-        })
     })
 }
 
